91eb914d60a29590c024b24b22c6598e1b05723a
romluc <romulofilhofilho@gmail.com>
2019-10-14 19:26:47 -0300

    Create TodoList component

---
+++ TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ToDo() {
+	return (
+		<>
+			<ul>
+				<li>ToDo Item</li>
+			</ul>
+
+			<input type='text' />
+			<button>New ToDo</button>
+		</>
+	);
+}
