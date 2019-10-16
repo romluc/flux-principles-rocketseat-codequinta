d7178acb833c335f5b39c6f493bb88d7fec34b59
romluc <romulofilhofilho@gmail.com>
2019-10-16 10:39:13 -0300

    Create store and reducer

--- redux-principles/src/App.js
+++ redux-principles/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Sidebar from './components/Sidebar';
 import Video from './components/Video';
-function App() {
-	return (
-		<div className='App'>
-			<Video />
-			<Sidebar />
-		</div>
-	);
+
+class App extends Component {
+	state = {
+		activeLesson: {}
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<Video activeLesson={this.state.activeLesson} />
+				<Sidebar modules={this.state.modules} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- redux-principles/src/components/Sidebar/index.js
+++ redux-principles/src/components/Sidebar/index.js
@@ -3,28 +3,7 @@ import React, { Component } from 'react';
 // import { Container } from './styles';
 
 export default class Sidebar extends Component {
-	state = {
-		modules: [
-			{
-				id: 1,
-				title: 'Starting React',
-				lessons: [
-					{ id: 1, title: 'First class' },
-					{ id: 2, title: 'Second class' }
-				]
-			},
-			{
-				id: 2,
-				title: 'Learning Redux',
-				lessons: [
-					{ id: 3, title: 'Third class' },
-					{ id: 4, title: 'Fourth class' }
-				]
-			}
-		]
-	};
 	render() {
-		const { modules } = this.state;
 		return (
 			<aside>
 				{modules.map(module => (
